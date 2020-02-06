20ecfe78c10481cd653718d3292f9d176cfb62f1
inejc <nejc.ilenic@gmail.com>
2020-02-07 00:42:04 +0100

    add dummy game and state definitions

--- tarok/src/tarok.h
+++ tarok/src/tarok.h
@@ -4,6 +4,7 @@
 
 namespace tarok {
 
+// game declaration
 class TarokGame : public open_spiel::Game {
 public:
     explicit TarokGame(const open_spiel::GameParameters& params);
@@ -11,7 +12,7 @@ public:
     int NumDistinctActions() const override;
     std::unique_ptr<open_spiel::State> NewInitialState() const override;
     // int MaxChanceOutcomes() const override;
-    int NumPlayers() const override { return 3; }
+    int NumPlayers() const override;
     double MinUtility() const override;
     double MaxUtility() const override;
     std::shared_ptr<const Game> Clone() const override;
@@ -19,6 +20,7 @@ public:
     int MaxGameLength() const override;
 };
 
+// state declaration
 class TarokState : public open_spiel::State {
 public:
     explicit TarokState(std::shared_ptr<const open_spiel::Game> game);
@@ -33,13 +35,12 @@ public:
         open_spiel::Player player,
         const std::string& action_str
     ) const override;
-    virtual std::string ToString() const override;
-    virtual bool IsTerminal() const override;
+    std::string ToString() const override;
+    bool IsTerminal() const override;
     std::vector<double> Returns() const override;
     std::string InformationStateString(
         open_spiel::Player player
     ) const override;
-    // std::string ObservationString(open_spiel::Player player) const override;
     std::unique_ptr<State> Clone() const override;
     open_spiel::ActionsAndProbs ChanceOutcomes() const override;
 
