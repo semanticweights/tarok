5da9fa87e6a5bd940a3c13fd71b3a215ef98e358
inejc <nejc.ilenic@gmail.com>
2020-02-06 21:22:45 +0100

    add tarok game and state declarations

--- tarok/src/tarok.cpp
+++ tarok/src/tarok.cpp
@@ -3,9 +3,4 @@
 #include "open_spiel/spiel.h"
 
 namespace tarok {
-
-TarokGame::TarokGame() {}
-
-std::string TarokGame::GetName() { return "Welcome to the tarok game :)"; }
-
 } // namespace tarok
--- tarok/src/tarok.h
+++ tarok/src/tarok.h
@@ -4,10 +4,39 @@
 
 namespace tarok {
 
-class TarokGame {
+class TarokGame : public open_spiel::Game {
 public:
-    TarokGame();
-    std::string GetName();
+    explicit TarokGame(const open_spiel::GameParameters& params);
+
+    int NumDistinctActions() const override;
+    std::unique_ptr<open_spiel::State> NewInitialState() const override;
+    // int MaxChanceOutcomes() const override;
+    int NumPlayers() const override { return 3; }
+    double MinUtility() const override;
+    double MaxUtility() const override;
+    std::shared_ptr<const Game> Clone() const override;
+    // double UtilitySum() const override;
+    int MaxGameLength() const override;
+};
+
+class TarokState : public open_spiel::State {
+public:
+    explicit TarokState(std::shared_ptr<const open_spiel::Game> game);
+
+    open_spiel::Player CurrentPlayer() const override;
+    std::vector<open_spiel::Action> LegalActions() const override;
+    std::string ActionToString(open_spiel::Player player, open_spiel::Action action_id) const override;
+    open_spiel::Action StringToAction(open_spiel::Player player, const std::string& action_str) const override;
+    virtual std::string ToString() const override;
+    virtual bool IsTerminal() const override;
+    std::vector<double> Returns() const override;
+    std::string InformationStateString(open_spiel::Player player) const override;
+    // std::string ObservationString(open_spiel::Player player) const override;
+    std::unique_ptr<State> Clone() const override;
+    open_spiel::ActionsAndProbs ChanceOutcomes() const override;
+
+protected:
+    void DoApplyAction(open_spiel::Action action_id) override;
 };
 
 } // namespace tarok
