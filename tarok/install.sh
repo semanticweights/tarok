05bcab65118279a997fdd4db7d1acf2d542d79b3
inejc <nejc.ilenic@gmail.com>
2020-05-20 00:00:08 +0200

    remove building unnecessary targets

--- tarok/install.sh
+++ tarok/install.sh
@@ -4,9 +4,16 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "$0")" >/dev/null 2>&1; pwd -P)"
 BUILD_DIR="${SCRIPT_DIR}/../build"
 
-echo "Installing open spiel dependencies"
+echo "Downloading open spiel dependencies"
 cd ${SCRIPT_DIR}/libs/open_spiel
-./install.sh
+# this needs to be in sync with the current open spiel version's install.sh script
+[[ -d "./pybind11" ]] || git clone -b 'v2.2.4' --single-branch --depth 1 https://github.com/pybind/pybind11.git
+[[ -d open_spiel/games/bridge/double_dummy_solver ]] || \
+  git clone -b 'develop' --single-branch --depth 1 https://github.com/jblespiau/dds.git  \
+  open_spiel/games/bridge/double_dummy_solver
+[[ -d open_spiel/abseil-cpp ]] || \
+  git clone -b 'master' --single-branch --depth 1 https://github.com/abseil/abseil-cpp.git \
+  open_spiel/abseil-cpp
 cd ${SCRIPT_DIR}
 
 echo "Building the tarok project"
