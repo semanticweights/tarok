cd27e9cdbba111b11a4f1dccd03dfea7e1c69557
inejc <nejc.ilenic@gmail.com>
2020-05-19 22:17:50 +0200

    add files for contracts

---
+++ tarok/src/contracts.cpp
@@ -0,0 +1,9 @@
+/* Copyright 2020 Semantic Weights. All rights reserved. */
+
+#include "src/contracts.h"
+
+namespace tarok {
+
+// todo
+
+}  // namespace tarok
---
+++ tarok/src/contracts.h
@@ -0,0 +1,9 @@
+/* Copyright 2020 Semantic Weights. All rights reserved. */
+
+#pragma once
+
+namespace tarok {
+
+// todo
+
+}  // namespace tarok
